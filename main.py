a2a0ea72fa45482fb4d809dfeed846ef80d705f0
Kuya Takami <kuya@gwu.edu>
2017-03-11 14:23:31 -0500

    command by power

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import progressbar
 from time import sleep
 
+
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = '/dev/ttyACM0'
@@ -18,10 +19,12 @@ time.sleep(2)
 
 time_init = time.time()
 t_cmd, t_interval = 0., 0.
-t_f = 15.
+t_f = 120
 cycle = 1.
-cmd_min = 60
-cmd_max = 160
+cmd_min = 80
+cmd_max = 244
+p_max = 83.
+p_min = 10.
 
 ser.write(str(cmd_min).zfill(3))
 z = []
@@ -29,6 +32,12 @@ z = []
 cmd = 0
 dt_f = 0.
 v_in = 0
+
+coef =np.array([-1.12e-9,   2.91e-7,  -2.76e-5  , 1.05e-3  ,-1.34e-3 , -9.94e-1, 2.69e1  ,-1.23e2])
+coef = np.array([2.14,  69.83])
+
+p2cmd = np.poly1d(coef)
+print 'min power: '+ str(p2cmd(p_min)) +' max power: '+ str(p2cmd(p_max))
 
 
 bar = progressbar.ProgressBar(maxval=100, \
@@ -43,33 +52,34 @@ ser.readline()
 #     break
 v_in_i, cmd_in_i, dt_in_i = 0, 0 ,0
 x = []
+
 while(True):
   save_flag = 1;
-  dt = ser.readline()
+  serial_in = ser.readline()
   t_now = time.time()-time_init
   t_cmd = t_now - t_interval
   # print dt
-  if dt[0] == 'V':
-    v_in = dt[8:]
+  if serial_in[0] == 'V':
+    v_in = serial_in[8:]
     try:
       v_in_i = int(v_in)
     except ValueError:
       save_flag = 0
-      print( 'voltage value error: ' + str(dt))
+      print( 'voltage value error: ' + str(serial_in))
       pass
     # print dt
     # print v_in
-  elif dt[0] == 'c':
-    cmd_in = dt[4:]
+  elif serial_in[0] == 'c':
+    cmd_in = serial_in[4:]
     try:
       cmd_in_i = int(cmd_in)
     except ValueError:
       save_flag = 0
-      print( 'cmd value error: ' + str(dt))
+      print( 'cmd value error: ' + str(serial_in))
       pass
     # print cmd_in
-  elif dt[0] == 'I':
-    dt_str = dt[4:]
+  elif serial_in[0] == 'I':
+    dt_str = serial_in[4:]
     try:
       x = [int(i) for i in dt_str.split()]
       # current =
@@ -77,39 +87,41 @@ while(True):
       # rpm =
     except ValueError:
       save_flag = 0
-      print( 'I2C read value error: ' + str(dt))
+      print( 'I2C read value error: ' + str(serial_in))
 
-  elif len(dt)>0:
-    dt_in = dt
+  elif len(serial_in)>0:
+    dt_in = serial_in
     try:
       dt_in_i = int(dt_in)
     except ValueError:
       save_flag = 0
-      print( 'dt value error: ' + str(dt))
+      print( 'dt value error: ' + str(serial_in))
       pass
   if t_now>4 and save_flag:
     z.append([cmd_in_i,dt_in_i,v_in_i,x[0],x[3],x[-1]])
 
-  if t_now > 1.5 and t_cmd > 0.05:
-    mot_cmd = cmd_min + (cmd_max-cmd_min)/t_f*t_now #
+  if t_now > 1. and t_cmd > 0.01:
+    p_d = p_min + (p_max-p_min)/t_f*t_now #
+    mot_cmd = p2cmd(p_d)
+    # mot_cmd = cmd_min + (cmd_max-cmd_min)/t_f*t_now #
     # mot_cmd = cmd_min + 1./2*(cmd_max-cmd_min)*(1.+np.sin(-np.pi/2+2.*np.pi*t_now/t_f*cycle))
     cmd =  int(mot_cmd)
     cmd_str = str(cmd).zfill(3)
     ser.write(cmd_str)
     t_interval = time.time() - time_init
-  elif t_now < 1:
-    cmd = cmd_min
-    ser.write(str(cmd).zfill(3))
+  # elif t_now < 1:
+  #   cmd = cmd_min
+  #   ser.write(str(cmd).zfill(3))
 
-  if cmd >= cmd_max or t_now > t_f:
+  if cmd > cmd_max and t_now > t_f:
     ser.write('000')
     time.sleep(1)
     break
   # print int(100.*(cmd-cmd_min)/(cmd_max-cmd_min))
-  bar.update(int(100.*t_now/t_f))
+  # bar.update(int(100.*t_now/t_f))
 
 ser.write('000')
 bar.finish()
 ser.close()
 z = np.array(z)
-np.save('test.npy',z)
+np.save('power_command_3.npy',z)
--- post_proc.py
+++ post_proc.py
@@ -1,35 +1,107 @@
 #!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
-import pdb
+import pdb, sys
+from decimal import Decimal
 
-z = np.load('test.npy')
+z = np.load('calibration.npy')
+z = np.load('power_command_3.npy')
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-N = 2
-# pdb.set_trace()
-plt.subplot(2, 2, 1)
-plt.plot(1./z[:,1]*60/N*1e6,'.')
-plt.ylabel('RPM')
-plt.subplot(2, 2, 2)
-plt.plot(z[:,2],'.-')
-plt.ylabel('grams')
-plt.subplot(2, 2, 3)
-plt.plot(z[:,0],'.-')
-plt.ylabel('cmd')
+N = 1 # number of p-magnet
+t = np.linspace(0,120,z.shape[0])
+power = z[:,-3]*z[:,-1]*0.01
+cmd = z[:,0]
+throttle = 9.81*1e-3*z[:,2]
 rpm = 1./z[:,1]*60/N*1e6
-plt.subplot(2, 2, 4)
-plt.plot(rpm,z[:,2],'.-')
+cmd_power = np.poly1d(np.array([  0.44, -25.69]))
 
-z = np.polyfit(rpm,z[:,2],2)
-rpm_fit = np.linspace(2000,5500,100)
-p = np.poly1d(z)
-print z
-plt.plot(rpm_fit,p(rpm_fit),'--r')
-plt.ylabel('Throttle')
+p_max = 85.
+p_min = 10.
+cmd_power_in = np.linspace(p_min,p_max,z.shape[0])
+
+fig, ax1 = plt.subplots()
+plt.grid()
+lrpm = ax1.plot(t, rpm,'g.',label='RPM')
+ax1.set_ylim([3000, 5600])
+ax1.set_ylabel('RPM')
+ax1.set_xlabel('time (s)')
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+ax2 = ax1.twinx()
+lpow = ax2.plot(t,power,'r.',label='Power')
+lpcmd = ax2.plot(t,cmd_power_in,'b.',label='Power$_{ref}$')
+ax2.set_ylim([p_min,100])
+plt.ylabel('Power (watt)')
+
+lns = lrpm+lpow+lpcmd
+labs = [l.get_label() for l in lns]
+plt.legend(lns, labs, loc=2)
+plt.savefig('power_ramp.png')
+
+plt.figure()
+plt.plot(t,cmd)
+plt.plot(t,power)
+
+
+
+
+
+plt.figure()
+plt.subplot(2, 2, 2)
+plt.grid()
+plt.plot(power,throttle,'.')
+plt.ylabel('Throttle [N]')
+plt.xlabel('Power [watt]')
+
+plt.subplot(2, 2, 3)
+plt.grid()
+plt.plot(power,cmd,'.')
+plt.ylabel('cmd')
+
+z_fit = np.polyfit(cmd,power,1)
+np.set_printoptions(precision=2)
+print 'cmd to power: ' + str(z_fit)
+
+
+z_fit = np.polyfit(power,cmd,1)
+power_fit = np.linspace(power.min(),power.max(),100)
+p = np.poly1d(z_fit)
+np.set_printoptions(precision=2)
+print z_fit
+plt.plot(power_fit,p(power_fit),'--r')
+
+
+plt.subplot(2, 2, 4)
+plt.grid()
+plt.plot(rpm,throttle,'.-')
+plt.ylabel('Throttle [N]')
 plt.xlabel('rpm')
+
+
+z_fit = np.polyfit(rpm,throttle,2)
+rpm_fit = np.linspace(2000,5500,100)
+p = np.poly1d(z_fit)
+np.set_printoptions(precision=2)
+print z_fit
+plt.plot(rpm_fit,p(rpm_fit),'--r')
+plt.text(2400, 4, z_fit, fontsize=12)
 
 plt.savefig('fit.png')
 # pp.close()
+plt.show()
+
+
+fig, ax1 = plt.subplots()
+# pdb.set_trace()
+# t = np.linspace(0,z.shape[0]/100,z.shape[0])
+ax1.plot(t, z[:,-1]*0.1,'.',label='Voltage')
+plt.ylabel('Voltage [V]')
+plt.xlabel('Time [s]')
+plt.grid()
+ax2 = ax1.twinx()
+ax2.plot(t, z[:,-3]*0.1,'r.',label='Current')
+plt.ylabel('Current [C]')
+# plt.xlim([0, 68])
+plt.tight_layout()
 plt.show()
